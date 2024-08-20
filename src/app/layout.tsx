67ec30d536d004d93e274e6031ce163294502f9c
zohaib ismail <Clamboy076@gmail.com>
2024-08-20 10:54:08 +0500

    root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <meta name="google-adsense-account" content="ca-pub-4804828229925861"/>
-        <Adsence pId="ca-pub-4804828229925861"/>
+      <Adsence pId="ca-pub-4804828229925861"/>
       </head>
       
       <body className={inter.className}>{children}</body>
